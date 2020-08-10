f7d183daff470251c2964036c8100638783c3d53
DEVA NANDANAN B H <63557312+Chris-Carlo@users.noreply.github.com>
2020-08-10 22:51:43 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,17 +1,17 @@
 class Translation(object):
     START_TEXT = """Hello,
 I am Renamer Bot!
-I am a bot made by Akshay H. 
+I am a bot made by Chris Carlo.
 <b>Please send me any Telegram file and reply to that file to /rename new name.extension</b>
 
 /help for more details.."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "/help for Details"
-    DOWNLOAD_START = "Downloading in my local servers..."
+    DOWNLOAD_START = "Downloading to my local servers..."
     UPLOAD_START = "Uploading please wait..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/Chris_Carlo'>@Chris_Carlo</a>"
@@ -27,7 +27,7 @@ I am a bot made by Akshay H.
 1. Send me any Telegram File.
 2. Reply to that message to /rename new name.extension
 --------
-Support : @darklester @Chris_Carlo"""
+Support : @moneyheistoftelegram"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
